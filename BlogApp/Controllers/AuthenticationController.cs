f0cde7b27e7c21a73bba98d0f31eaddb2c2ca442
Ahmad Abdullah <l190991@lhr.nu.edu.pk>
2024-11-18 17:22:30 +0500

    Making static register and login page

--- BlogApp/Controllers/AuthenticationController.cs
+++ BlogApp/Controllers/AuthenticationController.cs
@@ -8,5 +8,15 @@ namespace BlogApp.Controllers
         {
             return View();
         }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login() 
+        {
+            return View();
+        }
     }
 }
