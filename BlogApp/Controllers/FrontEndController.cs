92d126f856affc1b4f46d1d7f2228d5f34857fde
Ahmad Abdullah <l190991@lhr.nu.edu.pk>
2024-11-29 22:45:51 +0500

    making front end index page half completed

---
+++ BlogApp/Controllers/FrontEndController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Controllers
+{
+    public class FrontEndController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -32,6 +32,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=FrontEnd}/{action=Index}/{id?}");
 
 app.Run();
