7b2b7be27181e59c80e0097ee23b51110552b2a1
Ahmad Abdullah <l190991@lhr.nu.edu.pk>
2024-12-02 23:26:01 +0500

    completed front end index page

--- BlogApp/wwwroot/js/frontendIndex.js
+++ BlogApp/wwwroot/js/frontendIndex.js
@@ -1,16 +1,1 @@
-﻿let currentIndex = 0;
-
-function moveSlide(direction) {
-    const slides = document.querySelectorAll('.carousel-item');
-    const totalSlides = slides.length;
-
-    // Update current index based on direction
-    currentIndex = (currentIndex + direction + totalSlides) % totalSlides;
-
-    // Update the transform property of the carousel to shift the slides
-    const carousel = document.querySelector('.carousel');
-    carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
-}
-
-// Optional: Automatically transition every 3 seconds
-setInterval(() => moveSlide(1), 3000);
+﻿
